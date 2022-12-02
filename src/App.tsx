a3c5d07edfe81079db7499979fd700acc60d4015
Louisss-Lam <louislam617@gmail.com>
2022-12-02 18:04:35 +0000

    feat: build the search ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,27 @@ const App = () => {
         justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg
         rounded drop-shadow-lg text-zinc-700
       ">
-        <h1>Weather Forecast</h1>
-        <p>Enter below a place you want to know the weather of and select an option from the dropdown</p>
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+
+        <p className="text-sm mt-2">
+          Enter below a place you want to know the weather of and select an option from the dropdown
+        </p>
+
+        <div>
+          <input 
+            type="text" 
+            value={'k'} 
+            className="px-2 py-1 rounded-l-md border-2 border-white" 
+          />
+
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500
+            hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer
+          ">
+            search
+          </button>
+        </div>
       </section>
     </main>
   )
