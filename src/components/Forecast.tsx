74a91371f45ccbb2c2aff1786d506f79e2bbcc74
Louisss-Lam <louislam617@gmail.com>
2022-12-06 16:06:52 +0000

    feat: Wind display setup

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,4 +1,8 @@
+import { getSunTime, getWindDirection } from "../helpers";
 import { forecastType } from "../types";
+import Sunrise from "./Icons/Sunrise";
+import Sunset from "./Icons/Sunset";
+import Tile from "./Tile";
 
 type Props = {
     data: forecastType
@@ -16,7 +20,7 @@ const Forecast = ({ data }: Props): JSX.Element => {
 
   return (
     <div className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24
-    h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg"
+    h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg"
     >
         <div className="mx-auto w-[300px]">
             <section className="text-center">
@@ -27,7 +31,53 @@ const Forecast = ({ data }: Props): JSX.Element => {
                 <h1 className="text-4xl font-extrabold">
                     <Degree temp={Math.round(today.main.temp)} />
                 </h1>
-                <p>{today.weather[0].description}</p>
+                <p className="text-sm">
+                    {today.weather[0].main}{' '}
+                    {today.weather[0].description}
+                </p>
+                <p className="text-sm">
+                    H: <Degree temp={Math.ceil(today.main.temp_max)} />
+                    {' '}
+                    L: <Degree temp={Math.floor(today.main.temp_min)} />
+                </p>
+            </section>
+
+            <section className="flex overflow-x-scroll mt-4 pb-2 mb-5">
+                {data.list.map((item, i) => (
+                    <div 
+                        className="inline-block text-center w-[50px] flex-shrink-0"
+                        key={i}>
+                        <p className='text-sm'>{i === 0 ? 'Now' : new Date(item.dt * 1000).getHours()}</p>
+                        <img 
+                            src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} 
+                            alt={`weather-icon-${item.weather[0].description}`} 
+                        />
+                        <p className="text-sm font-bold">
+                            <Degree temp={Math.round(item.main.temp)} />
+                        </p>
+                    </div>
+                ))}
+            </section>
+
+            <section className="flex flex-wrap justify-between text-zinc-700">
+                <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20
+                backdrop-blur-ls rounded drop-shadow-lg py-4 mb-5">
+                    <Sunrise /> <span className="mt-2">{getSunTime(data.sunrise)}</span>
+                </div>
+                <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20
+                backdrop-blur-ls rounded drop-shadow-lg py-4 mb-5">
+                    <Sunset /> <span className="mt-2">{getSunTime(data.sunset)}</span>
+                </div>
+
+                {/* wind */}
+                <Tile 
+                    icon="wind" 
+                    title="Wind" 
+                    info={`${Math.round(today.wind.speed)} km/h`}
+                    description={`${getWindDirection(Math.round(today.wind.deg))}, gust ${today.wind.gust.toFixed(1)} km/h`}
+                />
+
+                
             </section>
         </div>
     </div>
